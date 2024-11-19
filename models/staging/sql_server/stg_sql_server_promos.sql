01b806a82620aeaf63f76cc845df960d269e6c89
Francisco Javier Figueroa Rodríguez <baunieto@gmail.com>
2024-11-19 18:12:55 +0000

    primeros stages en silver

--- models/staging/sql_server/stg_sql_server_promos.sql
+++ models/staging/sql_server/stg_sql_server_promos.sql
@@ -1,19 +1,34 @@
 {{ config(materialized='table') }}
 
-WITH base AS (
+WITH t1 as(
+     SELECT 
+        *
+    FROM {{ source('sql_server_dbo', 'promos') }} 
+UNION ALL
+    SELECT
+    'no promo' AS promo_id, 
+    '0' AS discount,         
+    'inactivo' AS status,     
+    NULL AS _fivetran_deleted, 
+    CURRENT_TIMESTAMP AS _fivetran_synced 
+)
+
+
+,base AS (
     SELECT 
         *,
-        {{ dbt_utils.generate_surrogate_key(['promo_id']) }} AS promo_surr_key, -- Genera la clave surrogada
-        promo_id AS promo_desc -- Copia el valor de promo_id a promo_desc
-    FROM {{ source('sql_server_dbo', 'promos') }} -- Fuente original
+        {{ dbt_utils.generate_surrogate_key(['promo_id']) }} AS promo_surr_key, 
+        promo_id AS promo_desc 
+    FROM t1
 )
 
 SELECT 
-    promo_surr_key, -- Nueva clave surrogada
-    promo_id,       -- Columna original
-    promo_desc,     -- Nueva columna
-    discount,       -- Mantén el resto de las columnas
+    promo_surr_key, 
+    promo_desc,     
+    discount,       
     status,
     _fivetran_deleted,
     _fivetran_synced
-FROM base;
+FROM base
+
+
