87e1f4d73a99d55f9c51895c0f50c079a6b0e6d2
Francisco Javier Figueroa Rodríguez <baunieto@gmail.com>
2024-11-29 18:55:58 +0000

    stg2

--- models/staging/sql_server/stg_sql_server_users.sql
+++ models/staging/sql_server/stg_sql_server_users.sql
@@ -1,16 +1,19 @@
 {{ config(materialized='table') }}
 
  SELECT 
-        user_id,
-        UPDATED_AT,
-        ADDRESS_ID,
+        cast(user_id as varchar(36)) as user_id,
+        CONVERT_TIMEZONE('UTC', UPDATED_AT) AS UPDATED_AT,
+        cast(ADDRESS_ID as varchar(36)) as ADDRESS_ID,
         LAST_NAME,
-        CREATED_AT,
+        CONVERT_TIMEZONE('UTC', CREATED_AT) as CREATED_AT,
         PHONE_NUMBER,
         FIRST_NAME,
         EMAIL,
-       -- _FIVETRAN_DELETED,
-        _FIVETRAN_SYNCED
+        CASE
+        WHEN _FIVETRAN_DELETED is null then false
+        else true
+        END as _FIVETRAN_DELETED,
+        CONVERT_TIMEZONE('UTC', _FIVETRAN_SYNCED) AS _FIVETRAN_SYNCED_UTC
     FROM {{ source('sql_server_dbo', 'users') }} 
 
     -- borramos campo total_orders, ya que todo nulo y ver que hacer con fivetran_deleted ya que también todo nulo
