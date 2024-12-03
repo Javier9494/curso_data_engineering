ae1d9be8588be84fefdee1eda7594a193503955f
Francisco Javier Figueroa Rodríguez <baunieto@gmail.com>
2024-12-03 16:16:20 +0000

    GOLD

--- models/marts/core/dim_shipping_services.sql
+++
@@ -1,9 +0,0 @@
--- models/dimensions/dim_shipping_services.sql
-WITH base AS (
-    SELECT DISTINCT
-        shipping_service
-    FROM {{ ref('stg_sql_server_dbo__orders') }}
-)
-SELECT
-    shipping_service
-FROM base
